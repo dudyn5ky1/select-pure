77ee887a8f236a53d5f96b9bc620fcec5c0709a4
Maksym Dudynskyi <maksymddd@gmail.com>
2018-05-02 19:04:57 +0200

    Add eslint scripts

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,8 @@
 import Boilerplate from "./../src/index.js";
 
 describe("Boilerplate component", () => {
-  test("adds 1 and 2", () => {
+  test("adds 'str' and 'ing'", () => {
     const boilerplate = new Boilerplate();
-    expect(boilerplate.add(1, 2)).toBe(3);
+    expect(boilerplate.add("str", "ing")).toBe("string");
   });
 });
