33eb2d136af05764636e99ae3e5f985d5545f3dc
Maksym Dudynskyi <maksymddd@gmail.com>
2018-05-07 20:35:56 +0200

    gitflow-release-stash: 0.1.11

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from "rollup-plugin-babel";
 import eslint from "rollup-plugin-eslint";
 import uglify from "rollup-plugin-uglify";
 import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
   input: "src/index.js",
@@ -21,6 +22,7 @@ export default {
       main: true,
       browser: true,
     }),
+    commonjs(),
     babel({
       plugins: [
         "transform-object-rest-spread",
