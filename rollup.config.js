d1cf749ffc37c80a88b075d5d7aa1618dab461c8
dudyn5ky1 <hello@dudynskyi.com>
2021-04-26 17:07:57 +0200

    fix(general): small fixes

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import minifyHTML from "rollup-plugin-minify-html-literals";
 
-// The main JavaScript bundle for modern browsers that support
-// JavaScript modules and other ES2015+ features.
 const config = {
   input: "src/index.js",
   output: {
